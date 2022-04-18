4792e2447c146e33251924d68a0570db9175b16d
prom07 <64492760+prom07@users.noreply.github.com>
2022-04-18 17:28:44 +0300

    Update add-client.sh

--- add-client.sh
+++ add-client.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+#!/bash
 if [ $# -eq 0 ]
 then
-	echo "must pass a client name as an arg: add-client.sh new-client"
+        echo "При запуске скрипта необходимо указать имя клиента: add_client.sh new-client"
 else
-	echo "Creating client config for: $1"
-	mkdir -p clients/$1
-	wg genkey | tee clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
-	key=$(cat clients/$1/$1.priv) 
-	ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
-	FQDN=$(hostname -f)
-  SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
-  cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
-	echo $ip > last-ip.txt
-	cp SETUP.txt clients/$1/SETUP.txt
-	tar czvf clients/$1.tar.gz clients/$1
-	echo "Created config!"
-	echo "Adding peer"
-	sudo wg set wg0 peer $(cat clients/$1/$1.pub) allowed-ips $ip/32
-	echo "Adding peer to hosts file"
-	echo $ip" "$1 | sudo tee -a /etc/hosts
-	sudo wg show
-	qrencode -t ansiutf8 < clients/$1/wg0.conf
+        echo "Creating client config for: $1"
+        wg genkey | tee $1.priv | wg pubkey > $1.pub
+        key=$(cat $1.priv)
+        ip_serv_ext=$(wget -qO- ipinfo.io/ip)
+        ip="170.21.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
+        SERVER_PUB_KEY=$(cat public_vpa.key)
+        cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$ip_serv_ext"'|' > $1.conf
+        echo $ip > last-ip.txt
+        echo "Created config!"
+        echo "Adding peer"
+        sudo wg set wg0 peer $(cat $1.pub) allowed-ips $ip/32
+        echo "Adding peer to hosts file"
+        echo $ip" "$1 | sudo tee -a /etc/hosts
+        sudo wg show
+        echo "[Peer]
+#$1
+PublicKey = $key
+AllowedIPs = $ip/32" | sudo tee -a /etc/wireguard/wg0.conf
+rm -f $1.pub
+rm -f $1.priv
+qrencode -t ansiutf8 < $1.conf
 fi
