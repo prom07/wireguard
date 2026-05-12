1fb8bbbc6eb2f622127c8da7a94a46dd34bd7a86
prom07 <64492760+prom07@users.noreply.github.com>
2026-05-12 21:47:22 +0300

    Update add-client.sh

--- add-client.sh
+++ add-client.sh
@@ -9,14 +9,13 @@ else
         wg genkey | tee $1.priv | wg pubkey > $1.pub
         key=$(cat $1.priv)
         ip_serv_ext=$(wget -qO- ipinfo.io/ip)
-        ip="170.21.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
-        SERVER_PUB_KEY=$(cat server_public_key)
+        ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
+        SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
         cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$ip_serv_ext"'|' > $1.conf
         echo $ip > last-ip.txt
         echo "Created config!"
         echo "Adding peer"
-        #sudo wg set wg0 peer $(cat $1.pub) allowed-ips $ip/32
-        sudo wg set wg0 peer $(cat $1.pub) allowed-ips 0.0.0.0/0
+        sudo wg set wg0 peer $(cat $1.pub) allowed-ips $ip/32
         echo "Adding peer to hosts file"
         echo $ip" "$1 | sudo tee -a /etc/hosts
         sudo wg show
